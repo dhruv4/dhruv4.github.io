3ff0930a9f7fa57a2bdf9469364aa2386c7e2985
Dhruv Gupta <dhruv4@gmail.com>
2015-07-21 00:40:02 -0400

    skills

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
 });
+$('ul#navmobile.side-nav li a').click(function(){
+	console.log("clicked");
+	if($("#proj-tab").hasClass('active')){
+		$("#proj-tab").removeClass("active");
+		$("#proj-tab .collapsible-body").hide();
+	}
+});
