d2ac9f4dd53d92b8080485d037c6c7cae195da10
Dhruv Gupta <dhruv4@gmail.com>
2015-08-23 17:07:04 -0400

    Updated website with stuff

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
+    $(".button-collapse").sideNav();
 });
 $('ul#navmobile.side-nav li a').click(function(){
-	console.log("clicked");
 	if($("#proj-tab").hasClass('active')){
 		$("#proj-tab").removeClass("active");
 		$("#proj-tab .collapsible-body").hide();
